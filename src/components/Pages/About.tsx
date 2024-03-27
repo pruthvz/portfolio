8e35cb31566e2f257af9b7b2caa1a8bea469e8c1
pruthvi10 <pruthvimohanlal10@gmail.com>
2024-03-27 18:49:02 +0000

    added about me

--- src/components/Pages/About.tsx
+++ src/components/Pages/About.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { TypeAnimation } from "react-type-animation";
 
 function About() {
   return (
@@ -32,6 +33,49 @@ function About() {
           <h1 class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
             About me
           </h1>
+        </div>
+      </section>
+      <section class="bg-white dark:bg-base-100">
+        <div class="gap-8 items-center py-8 px-4 mx-auto max-w-screen-xl xl:gap-16 md:grid md:grid-cols-2 sm:py-16 lg:px-6">
+          <img
+            class="w-full dark:hidden"
+            src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/cta/cta-dashboard-mockup.svg"
+            alt="picture of me"
+          />
+          <img
+            class="w-full hidden dark:block"
+            src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/cta/cta-dashboard-mockup-dark.svg"
+            alt="picture of me"
+          />
+          <div class="mt-4 md:mt-0">
+            <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">
+              I'm Pruthvi Mohanlal
+            </h2>
+            <p class="mb-6 font-light text-gray-500 md:text-lg dark:text-gray-400">
+              Flowbite helps you connect with friends and communities of people
+              who share your interests. Connecting with your friends and family
+              as well as discovering new ones is easy with features like Groups.
+            </p>
+
+            <div class="pt-5">
+              <a
+                href=""
+                class="p-3 pl-10 pr-10 bg-primary text-white rounded-full"
+              >
+                <i class="fa-solid fa-arrow-down"></i>
+              </a>
+              <a href="" class="p-3 ml-2  bg-gray-600 rounded-full text-white">
+                <i class="fa-brands fa-github "></i>
+              </a>
+              <a href="" class="p-3 ml-2 bg-gray-600 rounded-full text-white">
+                <i class="fa-brands fa-google-play"></i>
+              </a>
+
+              <a href="" class="p-3 ml-2 bg-gray-600 rounded-full text-white">
+                <i class="fa-brands fa-x-twitter "></i>
+              </a>
+            </div>
+          </div>
         </div>
       </section>
     </div>
